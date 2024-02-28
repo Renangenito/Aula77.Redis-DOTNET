17b3e66e73a43e1baa901a61fe775b451627ab12
renan lima <renangenito@hotmail.com>
2024-02-28 12:46:00 -0300

    Subindo o projeto completo

---
+++ Aula77.Redis/Model/PaisModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Aula77.Redis.Model
+{
+    public class PaisModel
+    {
+        //public string Name { get; set; }
+        //public string Capital { get; set; }
+        //public string Region { get; set; }
+        //public string SubRegion { get; set; }
+
+        [JsonProperty(propertyName: "name")]
+        public string Nome{ get; set; }
+        public string Capital { get; set; }
+
+        [JsonProperty(propertyName: "Region")]
+        public string Regiao{ get; set; }
+
+        [JsonProperty(propertyName: "SubRegion")]
+        public string SubRegiao { get; set; }
+    }
+}
